6342e64249c0702003ab193d742be4e2e9f76295
pemurtok-web <pemurtok@gmail.com>
2026-09-06 23:11:39 +0300

    Feat: switch to 100% static SSG for zero Netlify serverless costs and setup Build Hook

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   integrations: [
     tailwind(),
     react(),

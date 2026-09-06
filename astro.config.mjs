57d7b38dde1b93e2e15fa72333a2cbec2333aa2d
pemurtok-web <pemurtok@gmail.com>
2026-09-06 22:40:53 +0300

    Feat: enable Netlify SSR mode for instant live updates on Sanity publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind(),
     react(),
